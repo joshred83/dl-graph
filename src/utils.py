fa57b48ce4b0d3fc107d9f826a9a9ea396505562
Joshua Farina <johndoe@example.com>
2025-04-18 00:58:29 -0400

    dataloading utilities

--- src/utils.py
+++ src/utils.py
@@ -1,86 +1,5 @@
-from torch_geometric.datasets import EllipticBitcoinDataset, EllipticBitcoinTemporalDataset
-from torch_geometric.loader import NeighborLoader, ClusterLoader
 import torch
-import torch_geometric as pyg
-from torch_geometric import transforms
 
-def load_elliptic(
-    root=None, 
-    force_reload=False,
-    use_aggregated=False,
-    use_temporal=False,
-    t=None
-):
-    """
-    Load the EllipticBitcoinDataset.
-
-    Paper: 
-        Anti-Money Laundering in Bitcoin: Experimenting with Graph Convolutional
-        Networks for Financial Forensics" <https://arxiv.org/abs/1908.02591>`_
-        paper.
-
-    Content:
-        This anonymized dataset is a transaction graph collected from the
-        Bitcoin blockchain. A node in the graph represents a transaction,
-        and an edge can be viewed as a flow of Bitcoins between transactions.
-        Each node has 166 features and is labeled as being created by a
-        "licit", "illicit", or "unknown" entity.
-
-    Nodes and Edges:
-        - 203,769 nodes and 234,355 edges.
-        - 2% (4,545) of the nodes are labeled class1 (illicit).
-        - 21% (42,019) are labeled class2 (licit).
-        - The remaining transactions are not labeled with regard to licit
-          versus illicit.
-
-    Features:
-        - 166 features per node.
-        - Each node has a time step (1 to 49), representing when a
-          transaction was broadcasted.
-        - Each time step contains a single connected component; there are
-          no edges connecting different time steps.
-        - The first 94 features represent local transaction information
-          (e.g., time step, number of inputs/outputs, transaction fee,
-          output volume, aggregated figures).
-        - The remaining 72 features are aggregated features, obtained using
-          transaction information one-hop backward/forward from the center
-          node (e.g., max, min, std, correlation coefficients).
-    
-    Adapted from the description at
-    https://www.kaggle.com/datasets/ellipticco/elliptic-data-set
-    
-    Args:
-        root (str): Directory to store the dataset.
-        force_reload (bool): Whether to reload the dataset.
-
-    Returns:
-        data: The loaded data object (a graph).
-    """
-    if use_temporal and t is None:
-        raise ValueError("Temporal data requires a time step (t) to be specified.")
-    # default root to the data directory if not specified
-    if root is None:
-        root = "../data/elliptic"
-
-    if use_temporal:
-        dataset = EllipticBitcoinTemporalDataset(root=root, force_reload=force_reload, t=t)
-    else:
-        dataset = EllipticBitcoinDataset(root=root, force_reload=force_reload)
-    data = dataset[0]
-
-    
-    if use_aggregated:
-        # data already contains aggregated features
-        pass
-    else:
-        # Remove the aggregated features, correcting for the temporal case
-        # where the first feature is the time step
-        data.x = data.x[:, 0:93]
-                        
-
-
-    summarize_graph(data)
-    return data
 
 def summarize_graph(data):
     """
@@ -131,6 +50,7 @@ def summarize_graph(data):
 
 if __name__ == "__main__":
     # Example usage
+    from loaders import load_elliptic
     data = load_elliptic(use_temporal=True, t=2)
     
     print(data)
